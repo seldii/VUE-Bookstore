e125d116f6434004eac62daa177caf6a3613beaa
seldii <seldaguzel@gmail.com>
2019-05-28 17:07:14 +0200

    functioning app

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,15 +1,84 @@
 <template>
   <div id="app">
-    <div class="row" v-for="(chunk, index) in bookChunks" :key="index">
-      <div class="col-md-4" v-for="(book, index) in chunk" :key="index">
-        <div class="card card-flip">
-          <div class="card-inner">
-            <div class="card-front">
-              <img :src="book.portada" alt="cover" class="img-thumbnail">
+    <div class="container">
+      <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand">
+            <img src="./assets/logo.jpg" class="img-thumbnail float-left" alt="logo">
+          </a>
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2 float-right"
+              type="text"
+              v-model="searchTerm"
+              placeholder="Search"
+              aria-label="Search"
+            >
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </nav>
+      </div>
+
+      <div class="row is-multiline">
+        <div class="col-md-4 is-one-third" v-for="(book, index) in filteredBooks" :key="index">
+          <div class="card card-flip">
+            <div class="card-inner">
+              <div class="card-front">
+                <img :src="book.portada" alt="cover" class="img-thumbnail">
+              </div>
+              <div class="card-back">
+                <h3>{{book.titulo}}</h3>
+                <p>{{book.descripcion}}</p>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  data-toggle="modal"
+                  :data-target="'#myModal' + index"
+                >Show more</button>
+              </div>
             </div>
-            <div class="card-back">
-              <h3>{{book.titulo}}</h3>
-              <p>{{book.descripcion}}</p>
+            <div class="modal" :id="'myModal' + index" tabindex="-1" role="dialog">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div :id="'car'+ index" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div
+                          class="carousel-item"
+                          :class="{'active' : book === filteredBooks[index] }"
+                          v-for="(b, index) in books"
+                          :key="index"
+                        >
+                          <img :src="b.detalle" alt="cover" class="img-thumbnail">
+                        </div>
+                      </div>
+                      <a
+                        class="carousel-control-prev"
+                        :href="'#car' + index"
+                        role="button"
+                        data-slide="prev"
+                      >
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a
+                        class="carousel-control-next"
+                        :href="'#car' + index"
+                        role="button"
+                        data-slide="next"
+                      >
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -26,7 +95,8 @@ export default {
   data() {
     return {
       books: [],
-      loading: false
+      loading: false,
+      searchTerm: ""
     };
   },
   mounted() {
@@ -35,8 +105,16 @@ export default {
       .then(res => (this.books = res.data.books));
   },
   computed: {
-    bookChunks() {
-      return _.chunk(Object.values(this.books), 3);
+    filteredBooks() {
+      return this.books.filter(book => {
+        let searchBookHere =
+          book.titulo.replace(/\s/g, "") + book.descripcion.replace(/\s/g, "");
+        let searchBook =
+          this.searchTerm.length === 0 ||
+          searchBookHere.toLowerCase().indexOf(this.searchTerm.toLowerCase()) >
+            -1;
+        return searchBook;
+      });
     }
   }
 };
@@ -53,13 +131,13 @@ export default {
 }
 .card-flip {
   background-color: transparent;
-  border: 1px solid #f1f1f1;
+  /* border: 1px solid #f1f1f1; */
 }
 
 /* This container is needed to position the front and back side */
 .card-inner {
   text-align: center;
-  transition: transform 0.8s;
+  transition: transform 1s;
   transform-style: preserve-3d;
 }
 
@@ -79,15 +157,25 @@ export default {
   transform: rotateY(0deg);
   background-color: #bbb;
   color: black;
-  border: 2px solid black;
+  /* border: 2px solid black; */
 }
 
 /* Style the back side */
 .card-back {
   position: absolute;
   top: 0;
+  height: 100%;
   background-color: whitesmoke;
   color: black;
   transform: rotateY(180deg);
+}
+
+nav {
+  width: 100%;
+}
+
+.navbar img {
+  width: 50%;
+  height: 50%;
 }
 </style>
